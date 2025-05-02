f6ef1ca187287ba08afdd8e251ae9c4ff38a3fe2
Hilary <hilaryvirginia49@gmail.com>
2025-05-01 20:18:58 -0600

    Primer ejemplo

---
+++ 1-event-loop-asincronicidad/app.js
@@ -0,0 +1,9 @@
+//funcion que multiplica un cuadrado
+function multiplicar(x,y){
+    return x*y
+}
+function mostrarCuadrado(x){
+    let r=multiplicar(x,x)
+    console.log(r);}
+
+    mostrarCuadrado(5);
