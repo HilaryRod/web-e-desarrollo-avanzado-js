15828f3afeedd09534619fc42dff37ab6e6bbe16
Hilary <hilaryvirginia49@gmail.com>
2025-06-06 00:57:30 -0600

    Proyecto clase 7

---
+++ 7.1-mi-exploracion-espacial/planetas.js
@@ -0,0 +1,33 @@
+const planetas = [
+    {
+      nombre: "Titán",
+      descripcion: "La luna más grande de Saturno, con lagos de metano.",
+      descubiertoEn: "1655"
+    },
+    {
+      nombre: "Próxima Centauri b",
+      descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+      descubiertoEn: "2016"
+    },
+    {
+      nombre: "TRAPPIST-1e:",
+      descripcion: "Uno de los siete planetas del sistema TRAPPIST-1, ubicado a unos 40 años luz de la Tierra. Se encuentra en la zona habitable y podría albergar agua líquida",
+      descubiertoEn: "2016"
+    },
+        {
+      nombre: "Kepler-442b:",
+      descripcion: "Un exoplaneta rocoso descubierto en 2015, ubicado a unos 1,100 años luz de la Tierra. Se considera uno de los mejores candidatos para ser habitable",
+      descubiertoEn: "2015"
+    },
+        {
+      nombre: "Wolf 1061c:",
+      descripcion: "Un exoplaneta rocoso que orbita la estrella Wolf 1061, a unos 14 años luz de la Tierra. Se encuentra en la zona habitable y es uno de los exoplanetas más cercanos con potencial para albergar vida",
+      descubiertoEn: "2010"
+    },
+    
+    
+    
+
+  ];
+  
+  module.exports = planetas;
